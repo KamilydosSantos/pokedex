5afa44e4824c968addb8dd88affc9bbb253a2129
kamily <kamily7803@gmail.com>
2023-06-30 17:44:40 -0300

    feat: list pokemons axios

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -1,10 +1,30 @@
 <template>
-    <p>Lista</p>
+    <section class="pokemon">
+        <div v-for="pokemon in pokemons" :key="pokemon">{{ pokemon.name }}</div>
+    </section>
 </template>
 
 <script>
-export default {
+import axios from "axios"
 
+export default {
+    name: "ListPokemons",
+    data() {
+        return {
+            pokemons: null,
+        }
+    },
+    methods: {
+        getPokemons() {
+            axios.get("https://pokeapi.co/api/v2/pokemon/?limit=50")
+            .then(response => {
+                this.pokemons = response.data.results;
+            });
+        }
+    },
+    created() {
+        this.getPokemons();
+    }
 }
 </script>
 
